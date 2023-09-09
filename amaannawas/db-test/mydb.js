410ab8762ed6ba455899b99a9a0720bc12b51393
amaannawas <amaannawas@gmail.com>
2023-09-09 09:40:28 +0530

    correction

--- amaannawas/db-test/mydb.js
+++ amaannawas/db-test/mydb.js
@@ -26,7 +26,7 @@ myExpences.run(`CREATE TABLE IF NOT EXISTS expences (
         console.log(this);
     })
 
-    myExpences.run('UPDATe expences SET type = $newType, amount = $newAmount WHERE id = $id', {
+    myExpences.run('UPDATE expences SET type = $newType, amount = $newAmount WHERE id = $id', {
             $newType : 'ACCOMODATION', 
             $newAmount : 85000, 
             $id : 9 
